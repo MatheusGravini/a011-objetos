d96ee5e261d679f869d7b66047fd511e584af9da
MatheusGravini <gravini.mello@gmail.com>
2022-09-05 19:25:52 -0300

    Exercício finalizado Objetos

---
+++ exercicio1/script.js
@@ -0,0 +1,25 @@
+const sacolao = []
+
+const fruta1 = {
+    nome:"Morango",
+    preco: 5.00,
+    disponibilidade: true
+}
+
+const fruta2 = {
+    nome:"Abacaxi",
+    preco: 7.50,
+    disponibilidade:true
+}
+
+const fruta3 = {
+    nome:"Laranja",
+    preco: 3.00,
+    disponibilidade:false
+}
+
+
+sacolao.push(fruta1,fruta2,fruta3)
+console.log(sacolao)
+
+
